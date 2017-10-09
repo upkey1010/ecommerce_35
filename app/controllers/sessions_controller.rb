62951fb2df8a34b92f53f36d051f0303818bcd8e
Tin Nguyen <upkey1010@gmail.com>
2017-10-09 15:10:30 +0700

    User login with remember

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
+      create_param user
       redirect_to user
     else
       flash.now[:danger] = t "login_error_msg"
@@ -13,7 +14,13 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def create_param user
+    params[:session][:remember_me] == Settings.checkbox.true ? remember(user) : forget(user)
   end
 end
